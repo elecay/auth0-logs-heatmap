d1ad6e1fed4e4e687dbd5e90d7d3043251aed5f6
Sebastián Rajo <elecay@gmail.com>
2019-09-05 10:54:20 +0200

    Small refactor on IPApi

--- src/services/ip.api.js
+++ src/services/ip.api.js
@@ -1,19 +1,20 @@
-let fakeCache = {};
-
 class IPApi {
   static API_BASE = "http://ip-api.com/json/";
+
+  constructor() {
+    this.fakeCache = JSON.parse(localStorage.getItem("ipLatLngCache")) || {};
+  }
 
   async getLatLng(ipAddress) {
     // We do this to avoid being banned from http://ip-api.com
     // If your IP gets banned, go to http://ip-api.com/docs/unban
-    let ipLatLngCache = JSON.parse(localStorage.getItem("ipLatLngCache"));
-    if (ipLatLngCache && ipLatLngCache[ipAddress]) {
-      return ipLatLngCache[ipAddress];
+    if (this.fakeCache[ipAddress]) {
+      return this.fakeCache[ipAddress];
     }
     let users = await fetch(IPApi.API_BASE + ipAddress);
     let data = await users.json();
-    fakeCache[ipAddress] = data;
-    localStorage.setItem("ipLatLngCache", JSON.stringify(fakeCache));
+    this.fakeCache[ipAddress] = data;
+    localStorage.setItem("ipLatLngCache", JSON.stringify(this.fakeCache));
     return data;
   }
 }
